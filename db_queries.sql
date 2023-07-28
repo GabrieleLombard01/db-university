975f5d9159df8de1d5289c2a3cae81210feba9dc
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-28 17:28:45 +0200

    STEP 8

--- db_queries.sql
+++ db_queries.sql
@@ -22,7 +22,7 @@ SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 SELECT COUNT(*) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-
+SELECT * FROM `teachers` WHERE `phone` IS NOT NULL;
 
 -- QUERY CON GROUP BY:
 
