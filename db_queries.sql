3f95e07de2674e37f44953149590ff65958740b8
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-28 17:25:14 +0200

    step 7

--- db_queries.sql
+++ db_queries.sql
@@ -19,7 +19,7 @@ SELECT * FROM `exams` WHERE TIME(`hour`) > '14:00' AND DATE(`date`) = '2020-06-2
 SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
